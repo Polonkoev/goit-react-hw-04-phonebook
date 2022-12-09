11fc03cab647e73275f51709bef477698e72e43a
Polonkoev <103013345+Polonkoev@users.noreply.github.com>
2022-12-10 02:52:07 +0600

    fixed finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,24 +6,24 @@ import ContactForm from './ContactForm/ContactForm';
 import css from './App.module.css';
 
 const App = () => {
-  let [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  let [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem('contacts')) ?? [{ id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },]
+  );
 
   let [filterContact, setFilterContact] = useState('');
 
 //При первом монтировании 
 
-useEffect(()=>{
-  const savedContacts = localStorage.getItem('contacts')
-  if (savedContacts) {
-    setContacts(contacts = JSON.parse(savedContacts))
+// useEffect(()=>{
+//   const savedContacts = localStorage.getItem('contacts')
+//   if (savedContacts) {
+//     setContacts(contacts = JSON.parse(savedContacts))
     
-  }
-},[])
+//   }
+// },[])
 
 //При обновлении
 useEffect(()=>{
